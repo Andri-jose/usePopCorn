7f8c143ad229519cf9d29d8fbfa1888bfc21d0af
Andri Salazar <andr-y2008@hotmail.com>
2025-01-07 11:41:16 -0300

    saved

--- src/Movies.js
+++ src/Movies.js
@@ -1,29 +1,36 @@
+import { useState } from "react";
 
+export function Movies({movies}){
 
-export function Movies(){
-  <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen1((open) => !open)}
-          >
-            {isOpen1 ? "–" : "+"}
-          </button>
-          {isOpen1 && (
-            <ul className="list">
-              {movies?.map((movie) => (
-                <li key={movie.imdbID}>
-                  <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                  <h3>{movie.Title}</h3>
-                  <div>
-                    <p>
-                      <span>🗓</span>
-                      <span>{movie.Year}</span>
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+  const [isOpen1, setIsOpen1] = useState(true);
+
+  return(
+      <div className="box">
+            <button
+              className="btn-toggle"
+              onClick={() => setIsOpen1((open) => !open)}
+            >
+              {isOpen1 ? "–" : "+"}
+            </button>
+            {isOpen1 && (
+              <ul className="list">
+                {movies?.map((movie) => (
+                  <li key={movie.imdbID}>
+                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                    <h3>{movie.Title}</h3>
+                    <div>
+                      <p>
+                        <span>🗓</span>
+                        <span>{movie.Year}</span>
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+      </div>
+  
+  )
+  
 }
 
