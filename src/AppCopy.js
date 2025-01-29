4538f5bffa109c9363dca63d9fd4fb790236abd9
Andri Salazar <andr-y2008@hotmail.com>
2025-01-28 23:21:19 -0300

    error-practice

--- src/AppCopy.js
+++ src/AppCopy.js
@@ -17,15 +17,31 @@ const KEY = "456851c1"
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-  const query = "interstellar";
+  const query = "fvvsd";
+  const [loading, setloading] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(function () {
     async function fetchMovies() {
+     try {setloading(true)
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
+
+      if(!res.ok) 
+        throw new Error("something went wrong");
+
       const data = await res.json();
+      if(data.Response === "False") throw new Error("Movie not found!");
       setMovies(data.Search);
+      
+
+      } catch (error) {
+        setError(error.message)
+        console.log(error.message)
+      } finally{
+        setloading(false);
+      }
     }
     fetchMovies();
   }, []);
@@ -40,7 +56,9 @@ export default function App() {
        </Nav>
       <Main>
         <Box>
-          <MoviesDetail movies={movies}/>
+          {!loading && !error && <MoviesDetail movies={movies}/>}
+          {loading && <Load/>}
+          {error && <ErrorMessage message={error} />}
         </Box>
         <Box>
           <MoviesWatchedDetail watched={watched}/>
@@ -53,4 +71,10 @@ export default function App() {
 }
 
 
+function Load(){
+  return <p className="loader">Loading...</p>
+}
 
+function ErrorMessage({message}){
+  return <p className="error"><span>❌</span> {message}</p>
+}
